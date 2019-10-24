76652145f7d323528aaeef988d1d0fa82052b50f
Mithun Biswas <bhoot.biswas@gmail.com>
2019-10-24 11:45:41 +0600

    Add initial files

---
+++ easy-digital-downloads-gateway-stripe.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Easy Digital Downloads Gateway Stripe
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     easy-digital-downloads-gateway-stripe
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Easy_Digital_Downloads_Gateway_Stripe
+ */
+
+// Your code starts here.
